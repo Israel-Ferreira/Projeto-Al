70bfd6c3d9fee7d8d8fe88f7bc8a1474a269bb1d
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2019-05-24 02:02:05 -0300

    Montando o layout da tela de login

--- front-end-al/src/components/LoginForm.vue
+++ front-end-al/src/components/LoginForm.vue
@@ -1,57 +1,60 @@
 <template>
-    <div class="login-form">
-        <form  class="md-layout" @submit.prevent="submitForm">
-            <md-card class="md-layout-item md-size-50">
-                <md-card-header>
-                    <div class="md-title">{{isLoginScreen ? 'Login' : 'Cadastrar'}}</div>
-                </md-card-header>
-                <md-field v-if="!isLoginScreen">
+  <div class="login-form">
+    <form class="md-layout" @submit.prevent="submitForm">
+        <div class="md-title">{{componentTitle}}</div>
+        <md-field>
+            <label>Usuário</label>
+            <md-input v-model="username"></md-input>
+        </md-field>
 
-                </md-field>
-                <md-field>
-                    <label>Email: </label>
-                    <md-input v-model="email" type="email"/>
-                </md-field>
-                <md-field>
-                    <label>Nome de Usuário: </label>
-                    <md-input v-model="password" type="password" />
-                </md-field>
-                <md-card-actions>
-                    <md-button type="submit" class="md-primary">Create user</md-button>
-                </md-card-actions>
-            </md-card>
-        </form>
-    </div>
+        <md-field>
+            <label>Email</label>
+            <md-input v-model="email" type="email"></md-input>
+        </md-field>
+
+        <md-field>
+            <label>Senha</label>
+            <md-input type="password" v-model="password"></md-input>
+            <span class="md-helper-text">Helper text</span>
+        </md-field>
+        <md-button type="submit" class="md-raised md-primary">Create user</md-button>
+    </form>
+  </div>
 </template>
+
+
+
+
+
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 const api = "http://localhost:6590";
 
 export default {
-    data: () => ({
-        username: "",
-        email: "",
-        password: "",
-        isLoginScreen: true
-    }),
-    methods: {
-        submitForm(){
-            let {username,email,password} = this
+  props: ["isLoginScreen"],
+  name: "login-form",
+  data: () => ({
+    username: "",
+    email: "",
+    password: ""
+  }),
+  methods: {
+    submitForm() {
+      let { username, email, password } = this;
 
-            if(this.isLoginScreen){
-                console.log({username,password})
-                axios.post()
-            }else{
-                console.log({email,username,password})
-            }
-        }
-    },
-    computed: {
-        titlePage(){
-            return 
-        }
+      if (this.isLoginScreen) {
+        console.log({ username, password });
+        axios.post();
+      } else {
+        console.log({ email, username, password });
+      }
     }
-    
-}
+  },
+  computed: {
+    componentTitle() {
+      return this.isLoginScreen ? "Login" : "Cadastrar";
+    }
+  }
+};
 </script>
--- front-end-al/src/views/Auth.vue
+++ front-end-al/src/views/Auth.vue
@@ -1,5 +1,19 @@
 <template>
     <div class="auth">
-        
+        <div>
+            <LoginForm :isLoginScreen="true" />
+        </div>
     </div>
 </template>
+
+<script>
+import LoginForm from '@/components/LoginForm'
+
+export default {
+    name: 'auth',
+    components: {
+        LoginForm
+    }
+}
+</script>
+
